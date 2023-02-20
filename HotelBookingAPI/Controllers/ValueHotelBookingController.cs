bfff28c5ae54c59f338fe1627133ce40d579593d
Jaseela93 <jaseelajamal93@gmail.com>
2023-02-20 07:34:21 +0400

    feb 20

---
+++ HotelBookingAPI/Controllers/ValueHotelBookingController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HotelBookingAPI.Models;
+using HotelBookingAPI.Data;
+
+namespace HotelBookingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueHotelBookingController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public ValueHotelBookingController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        // Create/Edit
+
+        [HttpPost]
+        public JsonResult CreateEdit(HotelBooking booking)
+        {
+            if(booking.Id == 0)
+            {
+                _context.Bookings.Add(booking);
+            }
+            else
+            {
+                var bookingInDb = _context.Bookings.Find(booking.Id);
+                if(bookingInDb==null)
+                {
+                    return new JsonResult(NotFound());
+                }
+
+                bookingInDb = booking;
+            }
+            _context.SaveChanges();
+
+            return new JsonResult(Ok(booking));
+        }
+
+    }
+}
